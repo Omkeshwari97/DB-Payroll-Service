cdb71c4f2179dafbb7774e48d8bbe73816ff8cac
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-10-29 00:48:20 +0530

    Added functionality to retrieve salary data for employee and based on data range

--- paroll_service.sql
+++ paroll_service.sql
@@ -25,3 +25,8 @@ insert into employee_payroll (name, salary, start) values
     ->  ('Suruchi', 50000, '2020-05-18'),
     ->  ('Mital', 60000, '2018-06-24');
 select *  from employee_payroll;
+
+#uc5
+select salary from employee_payroll where name='Omkeshwari';
+select * from employee_payroll
+    -> where start between cast('2020-01-01' as date) and date(now());
