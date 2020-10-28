7db14ac3a9f4e1f9b68ca0c6c289991a81661c09
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-10-29 00:57:04 +0530

    Added functionality to add gender column and update details

--- paroll_service.sql
+++ paroll_service.sql
@@ -31,3 +31,17 @@ select *  from employee_payroll;
 select salary from employee_payroll where name='Omkeshwari';
 select * from employee_payroll
     -> where start between cast('2020-01-01' as date) and date(now());
+
+#uc6
+alter table employee_payroll
+    -> add gender char(1)
+    -> after name;
+describe employee_payroll;
+update employee_payroll
+    -> set gender = 'F'
+    -> where name = 'Omkeshwari' or name = 'Suruchi';
+select *  from employee_payroll;
+update employee_payroll
+    -> set gender = 'M'
+    -> where name = 'Mital';
+select *  from employee_payroll;
