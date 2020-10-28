512213da2c8abd01606353b7c8d2db271eed50c5
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-10-29 00:24:37 +0530

    Added functionality to create employee_payroll table

--- paroll_service.sql
+++ paroll_service.sql
@@ -4,3 +4,13 @@ show databases;
 create database payroll_service;
 show databases;
 use payroll_service;
+
+#uc2
+create table employee_payroll(
+    -> id int not null auto_increment,
+    -> name varchar(100) not null,
+    -> salary double not null,
+    -> start date not null,
+    -> primary key (id)
+    -> );
+describe employee_payroll;
